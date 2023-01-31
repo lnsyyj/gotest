b320ed908a4d1195d8b888df2f15eaf652d0f5ad
JiangYu <lnsyyj@hotmail.com>
2023-01-31 18:22:45 +0800

    etcd test
    Signed-off-by: JiangYu <lnsyyj@hotmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -22,11 +22,78 @@ func main() {
 	}
 
 	kv := clientv3.NewKV(cli)
-	putResp, err := kv.Put(context.Background(), "/samba/global/", "value")
+	ctx := context.Background()
+
+	// put
+	putResp, err := kv.Put(ctx, "/samba/global/k1", "value")
 	if err != nil {
 		fmt.Errorf("put to etcd failed, err:%v, ", err)
 	}
 	fmt.Println("Revision:", putResp.Header.Revision)
 
-	fmt.Println("Hello, World!")
+	putResp, err = kv.Put(ctx, "/samba/global/k2", "value")
+	if err != nil {
+		fmt.Errorf("put to etcd failed, err:%v, ", err)
+	}
+	fmt.Println("Revision:", putResp.Header.Revision)
+
+	// get
+	getResp, err := kv.Get(ctx, "/samba/global/k1")
+	if err != nil {
+		fmt.Errorf("get etcd failed, err:%v, ", err)
+	}
+	fmt.Println("Revision:", getResp.Header.Revision)
+	fmt.Println("Kvs:", getResp.Kvs)
+
+	// get WithPrefix
+	getResp, err = kv.Get(ctx, "/samba/global/", clientv3.WithPrefix())
+	if err != nil {
+		fmt.Errorf("get etcd failed, err:%v, ", err)
+	}
+	fmt.Println("Revision:", getResp.Header.Revision)
+	fmt.Println("Kvs:", getResp.Kvs)
+
+	// lease
+	lease := clientv3.NewLease(cli)
+	grantResp, err := lease.Grant(ctx, 10)
+	fmt.Println("grantRestp:", grantResp.ID)
+	if err != nil {
+		fmt.Errorf("grant lease failed, err:%v, ", err)
+	}
+	kv.Put(ctx, "/samba/global/k3", "value", clientv3.WithLease(grantResp.ID))
+	keepResp, err := lease.KeepAliveOnce(ctx, grantResp.ID)
+	if err != nil {
+		fmt.Errorf("keep alive lease failed, err:%v, ", err)
+	}
+	fmt.Println("Revision:", keepResp.Revision)
+
+	// OP
+	ops := []clientv3.Op{
+		clientv3.OpPut("/samba/global/k4", "value"),
+		clientv3.OpGet("/samba/global/k4")}
+	for _, op := range ops {
+		_, err := cli.Do(ctx, op)
+		if err != nil {
+			fmt.Errorf("do etcd failed, err:%v, ", err)
+		}
+	}
+
+	// Txn 事务
+	txn := kv.Txn(ctx)
+	txn.If(
+		clientv3.Compare(clientv3.CreateRevision("/samba/global/k5"), "=", 0),
+	).Then(
+		clientv3.OpPut("/samba/global/k5", "value"),
+	).Else(
+		clientv3.OpGet("/samba/global/k5"),
+	).Commit()
+
+	// watch
+	watchChan := cli.Watch(ctx, "/samba/global/k1")
+	for wresp := range watchChan {
+		for _, ev := range wresp.Events {
+			fmt.Printf("Type:%v key:%v value:%v\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
+		}
+	}
+
 }
